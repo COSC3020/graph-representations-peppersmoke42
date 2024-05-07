c4fd60a30fa0601dc42c9fd9cc84595e0639f40f
peppersmoke42 <peterchr102@gmail.com>
2024-05-07 17:52:02 -0600

    Updated Credit

--- code.js
+++ code.js
@@ -1,7 +1,10 @@
 // Haha no credit is needed here
 // Once again, why was this so much easier than the previous assignment?
 // I mean it took me like 10 minutes, compared to like 6+ hours
-
+//
+// I do take this back actually, it taught me the concept of adjacency lists
+// and matrices. Once I understood the difference, then it took 10 minutes
+// to write this
 function convertToAdjList(adjMatrix) {
     const adjList = [];
     for (let i = 0; i < adjMatrix.length; i++) {
